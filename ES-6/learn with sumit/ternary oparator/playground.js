370a9a75be1584e350525b9418c29a3feb9bd115
adnan rony <adnanrony19@gmail.com>
2024-01-16 18:25:05 +0600

    ternary

---
+++ ES-6/learn with sumit/ternary oparator/playground.js
@@ -0,0 +1,24 @@
+
+
+
+// const age=18;
+
+// const type=(age>=18) ? "adult" :"not adult";
+
+// console.log(type);
+
+
+
+// const age = 1;
+
+// const type = (age >= 18) ?"adult" : (age <= 10) ? "child" : "young"
+
+// console.log(type);
+
+
+
+const a = 11;
+
+const access = a > 10 ? true : false;
+
+console.log(access);
