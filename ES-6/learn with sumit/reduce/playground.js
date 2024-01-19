0b0a3eceb5892c37acd983ccee96ff73a810d446
adnan rony <adnanrony19@gmail.com>
2024-01-19 20:52:53 +0600

    reduce

--- ES-6/learn with sumit/reduce/playground.js
+++ ES-6/learn with sumit/reduce/playground.js
@@ -4,7 +4,8 @@ const numbers = [1, 2, 3, 4];
 
 const result = numbers.reduce((preValue, currentValue,currentindex) => {
 
-    return preValue + currentValue
+    // return preValue + currentValue
+    return currentindex <2
 })
 
 console.log(result);
